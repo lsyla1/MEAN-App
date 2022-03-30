945b4ff1dcc7d9eac21a4e49341a5e7122664edf
lsyla1 <liridonsyla1000@gmail.com>
2022-03-30 21:11:01 +0200

    Change package

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("./db.js");
+const routes = require("./routes/routes.js");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: "http://localhost:4200" }));
+
+// LOCALLY
+//const port = 3000;
+
+//Setup for Heroku
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log("Server started at port : 3000"));
+
+app.use("/employees", routes);
