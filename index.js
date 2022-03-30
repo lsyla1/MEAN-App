14592139f779c6755876d1b0f34d7f170ec5274b
lsyla1 <liridonsyla1000@gmail.com>
2022-03-30 21:18:55 +0200

    Change package

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('./db.js');
-const routes = require('./routes/routes.js')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("./db.js");
+const routes = require("./routes/routes.js");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({origin: 'http://localhost:4200'}))
+app.use(cors({ origin: "http://localhost:4200" }));
+app.use(express.static("frontend/dist"));
 
-app.listen(3000, () => console.log('Server started at port : 3000'));
+// LOCALLY
+//const port = 3000;
 
-app.use('/employees', routes);
+//Setup for Heroku
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log("Server started at port : 3000"));
+
+app.use("/employees", routes);
